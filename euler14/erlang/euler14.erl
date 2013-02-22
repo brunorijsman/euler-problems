a1e63a5e5b985b2a25a092e9f66b239dd4a6f1c5
Bruno Rijsman <brunorijsman@gmail.com>
2013-02-22 11:07:16 -0500

    Problem 14

---
+++ euler14/erlang/euler14.erl
@@ -0,0 +1,46 @@
+%% Project Euler, problem 14
+%% 
+%% Find the longest sequence using a starting number under one million.
+
+-module(euler14).
+
+-export([solve/0]).
+
+next_in_series(Nr) ->
+    if
+        Nr rem 2 == 0 ->
+            Nr div 2;
+        true ->
+            3 * Nr + 1
+    end.
+
+series_length(StartNr) ->
+    if
+        StartNr == 1 ->
+            1;
+        true ->
+            1 + series_length(next_in_series(StartNr))
+    end.
+
+longest_series(MaxStartNr) ->
+    longest_series(1, MaxStartNr, 0, 0).
+
+longest_series(StartNr, MaxStartNr, Longest, LongestStartNr) ->
+    if
+        StartNr >= MaxStartNr ->
+            LongestStartNr;
+        true ->
+            Length = series_length(StartNr),
+            if
+                Length > Longest ->
+                    longest_series(StartNr + 1, MaxStartNr, Length, StartNr);
+                true ->
+                    longest_series(StartNr + 1, MaxStartNr, Longest, LongestStartNr)
+            end
+    end.
+
+solve() ->
+    solve(1000000).
+
+solve(N) ->
+    longest_series(N).
