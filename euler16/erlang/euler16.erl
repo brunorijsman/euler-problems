3db9225a6a504ce3f97f933bb4d3c7dab87577b6
Bruno Rijsman <brunorijsman@gmail.com>
2013-02-22 11:15:21 -0500

    Problem 16

---
+++ euler16/erlang/euler16.erl
@@ -0,0 +1,24 @@
+%% Project Euler, problem 16
+%% 
+%% What is the sum of the digits of the number 2^1000?
+
+-module(euler16).
+
+-export([solve/0]).
+
+power_of_2(N) ->
+    1 bsl N.
+
+digits_sum(N) ->
+    if 
+        N < 10 ->
+            N;
+        true ->
+            (N rem 10) + digits_sum(N div 10)
+    end.
+
+solve() ->
+    solve(1000).
+
+solve(N) ->
+    digits_sum(power_of_2(N)).
