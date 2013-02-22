51baad6bab23f92d8961b2e1d0c31f1a81ade397
Bruno Rijsman <brunorijsman@gmail.com>
2013-02-22 11:26:01 -0500

    Problem 20

---
+++ euler20/erlang/euler20.erl
@@ -0,0 +1,19 @@
+%% Project Euler, problem 20
+%% 
+%% Find the sum of digits in 100!
+
+-module(euler20).
+
+-export([solve/0]).
+
+fact(0) -> 1;
+fact(N) -> N * fact(N-1).
+
+sum_of_digits(N) when N < 10 -> N;
+sum_of_digits(N) -> (N rem 10) + sum_of_digits(N div 10).
+
+solve() ->
+    solve(100).
+
+solve(N) ->
+    sum_of_digits(fact(N)).
