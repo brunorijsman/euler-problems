87e42901870ac7faeb209ea3c675bbcc17751c68
Bruno Rijsman <brunorijsman@gmail.com>
2013-02-22 11:27:16 -0500

    Problem 21

---
+++ euler21/erlang/euler21.erl
@@ -0,0 +1,45 @@
+%% Project Euler, problem 21
+%% 
+%% Evaluate the sum of all amicable pairs under 10000.
+
+-module(euler21).
+
+-export([solve/0]).
+
+sum_of_divisors(N) -> 
+    sum_of_divisors(N, 1, N div 2, 0).
+
+sum_of_divisors(N, Try, MaxTry, Sum) ->
+    if 
+        Try > MaxTry ->
+            Sum;
+        N rem Try == 0 ->
+            sum_of_divisors(N, Try + 1, MaxTry, Sum + Try);
+        true ->
+            sum_of_divisors(N, Try + 1, MaxTry, Sum)
+    end.
+
+is_amicable(N) ->
+    CandidateOther = sum_of_divisors(N),
+    ShouldBeN = sum_of_divisors(CandidateOther),
+    (N == ShouldBeN) and (N /= CandidateOther).
+
+sum_of_amicable_numbers(N) ->
+    sum_of_amicable_numbers(1, N, 0).
+
+sum_of_amicable_numbers(Try, MaxTry, Sum) ->
+    IsAmicable = is_amicable(Try), 
+    if 
+        Try >= MaxTry ->
+            Sum;
+        IsAmicable ->
+            sum_of_amicable_numbers(Try + 1, MaxTry, Sum + Try);
+        true ->
+            sum_of_amicable_numbers(Try + 1, MaxTry, Sum)
+    end.
+
+solve() -> 
+    solve(10000).
+
+solve(N) -> 
+    sum_of_amicable_numbers(N).
